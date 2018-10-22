e6464473cc343bde7727f00f3fa9807bb9794611
Liguang Zhu <zlgzhuliguang@gmail.com>
2018-10-22 23:32:35 +1100

    add templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,17 @@
 from app import app
-
+from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    return "test page"
+    user = {'username':'Leon'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.htm', title='Home', user=user, posts=posts)
