7a7630536ea04d5313d72aebafe09a1dcab8ea2e
Leon_Zhu <liguang.zhu@team.telstra.com>
2018-10-23 21:20:25 +1100

    login

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
@@ -15,3 +16,12 @@ def index():
         }
     ]
     return render_template('index.htm', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash("Login request for user {}, remember_me={}".format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.htm',title='Sign In', form=form)
